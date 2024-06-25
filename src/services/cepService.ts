d83fdc8819066493bdaed47e9b51736f4f0a0a83
vitorbachini <vbachiniw@gmail.com>
2024-06-25 14:53:26 -0300

    feat: added error controler to cep service

--- src/services/cepService.ts
+++ src/services/cepService.ts
@@ -1,10 +1,24 @@
 import axios from 'axios';
+import ICep from '../interfaces/iCep';
 
-export const getZipCode = async (cep: string) => {
+export const getZipCode = async (cep: string): Promise<ICep> => {
     try {
         const response = await axios.get(
             `https://viacep.com.br/ws/${cep}/json`,
         );
-        return response.data;
-    } catch (error) {}
+        
+        if (response.data.error){
+            throw new Error('Cep not found!')
+        }
+
+        return{
+            uf: response.data.uf,
+            localidade: response.data.localidade,
+            logradouro: response.data.logradouro,
+            bairro: response.data.bairro
+        }
+
+    } catch (error) {
+        throw new Error(`Error fetching cep data`)
+    }
 };
