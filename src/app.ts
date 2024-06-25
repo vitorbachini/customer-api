3d6179af0cb37f7255853020c5dca76f9d4ae107
vitorbachini <vbachiniw@gmail.com>
2024-06-25 16:42:08 -0300

    feat: create error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import { customerRouter } from './routes/customerRoute';
+import { errorHandler } from './middlewares/errorMiddleware';
 
 export const app = express();
 
 app.use(express.json());
+app.use(errorHandler)
 
 app.use('/api/v1', customerRouter);
