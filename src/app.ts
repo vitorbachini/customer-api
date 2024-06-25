e61eef907b98709a91adf54278461392e9f9f8cb
vitorbachini <vbachiniw@gmail.com>
2024-06-25 16:29:27 -0300

    feat: updated app with customer router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import { customerRouter } from './routes/customerRoute';
+
 export const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1', customerRouter);
