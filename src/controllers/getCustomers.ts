ab1cd2a25542af37764d2d3cec3b2e34294b044d
vitorbachini <vbachiniw@gmail.com>
2024-06-26 11:06:36 -0300

    feat: create get customer by id controller

--- src/controllers/getCustomers.ts
+++ src/controllers/getCustomers.ts
@@ -9,3 +9,18 @@ export const getAllCustomers = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+export const getCustomerById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const customer = await Customer.findById(id);
+        if (!customer) {
+            return res
+                .status(404)
+                .json({ message: `No customer with id: ${id}` });
+        }
+        res.status(200).json(customer);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
