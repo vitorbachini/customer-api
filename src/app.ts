8b6e863916e3b5db25deda7105de0b5cd98a635a
vitorbachini <vbachiniw@gmail.com>
2024-06-27 14:08:18 -0300

    feat: create swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express'
 
 import { customerRouter } from './routes/customerRoute';
 import { errorHandler } from './middlewares/errorMiddleware';
+import swaggerDocs from './swagger.json'
 
 export const app = express();
 
 app.use(express.json());
 app.use(errorHandler)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use('/api/v1', customerRouter);
